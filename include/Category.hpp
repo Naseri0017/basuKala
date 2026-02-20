65b959f32a448739583aa18b28621866c3896973
Naseri0017 <mohammadnaseri.ub@gmail.com>
2026-02-20 16:27:03 +0330

    fixet category

---
+++ include/Category.hpp
@@ -0,0 +1,14 @@
+#ifndef CATEGORY_HPP
+#define CATEGORY_HPP
+
+enum CategoryType {
+    DIGITAL = 0,   // کالاهای دیجیتال
+    HOME = 1,      // لوازم خانگی
+    CLOTHING = 2,  // پوشاک
+    FOOD = 3,      // مواد غذایی
+    BOOK = 4       // کتاب
+};
+
+const int CATEGORY_COUNT = 5;
+
+#endif
